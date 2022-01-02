807065741f7a2cf20e08d2a45fcc60e99132ad7d
Saša Đurić <djsasa.mail@gmail.com>
2022-01-02 01:15:40 +0100

    Refactored index template

--- src/templates/index/index.template.ts
+++ src/templates/index/index.template.ts
@@ -1,7 +1,7 @@
-import TemplateBuilder from '../../builders/base-template.builder';
-import JSTemplateBuilder from '../../builders/js-template.builder';
-import { toImportPath } from '../../utils/path';
+import j from 'jscodeshift';
 import BaseTemplate from '../base.template';
+import { toImportPath } from '../../utils/path';
+import { constructTemplate } from '../../utils/template';
 
 class IndexTemplate extends BaseTemplate {
 	constructor(
@@ -12,22 +12,27 @@ class IndexTemplate extends BaseTemplate {
 	}
 
 	build() {
-		const template = new JSTemplateBuilder();
-		const { importPath } = this;
-		const exportName =
-			this.exportProp === 'default'
-				? '{ default }'
-				: this.exportProp === 'all'
-				? '*'
-				: this.exportProp;
+		const body = [];
 
-		template.insertExportStatement({
-			defaultExport: false,
-			exportName: exportName,
-			exportFrom: toImportPath(importPath),
-		});
+		if (this.exportProp === 'all') {
+			body.push(
+				j.exportAllDeclaration(
+					j.stringLiteral(toImportPath(this.importPath)),
+					null
+				)
+			);
+		} else {
+			body.push(
+				j.exportDeclaration(
+					false,
+					null,
+					[j.exportSpecifier(j.identifier('default'), null)],
+					j.literal(toImportPath(this.importPath))
+				)
+			);
+		}
 
-		return template.toString();
+		return constructTemplate(body);
 	}
 }
 
