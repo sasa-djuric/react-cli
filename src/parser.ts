40308739b9087e0cb7fa5ed6edf8eb275d6982e5
Saša Đurić <djsasa.mail@gmail.com>
2022-01-09 13:06:37 +0100

    Fixed issue with babel plugins

--- src/parser.ts
+++ src/parser.ts
@@ -1,16 +1,28 @@
 import { parseSync } from '@babel/core';
+import path from 'path';
+
+const nodeModulesPath = path.join(
+	__dirname,
+	process.env.NODE_ENV === 'development' ? '../' : '',
+	'node_modules'
+);
 
 export const parser = {
 	parse: (source: any) =>
 		parseSync(source, {
 			plugins: [
-				`@babel/plugin-syntax-jsx`,
-				`@babel/plugin-proposal-class-properties`,
+				path.join(nodeModulesPath, `@babel/plugin-proposal-class-properties`),
+				path.join(nodeModulesPath, `@babel/plugin-syntax-jsx`),
 			],
 			overrides: [
 				{
 					test: [`**/*.ts`, `**/*.tsx`],
-					plugins: [[`@babel/plugin-syntax-typescript`, { isTSX: true }]],
+					plugins: [
+						[
+							path.join(nodeModulesPath, `@babel/plugin-syntax-typescript`),
+							{ isTSX: true },
+						],
+					],
 				},
 			],
 			filename: 'source-file.tsx',
